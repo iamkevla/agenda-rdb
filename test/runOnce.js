9ada48a5d350531e2dee235045cff5d867175183
Kevin Vlahos <kevin.vlahos@gmail.com>
2016-02-28 13:34:50 +1100

    test run once

---
+++ test/runOnce.js
@@ -0,0 +1,59 @@
+/* globals before, describe, it, beforeEach, after, afterEach */
+var expect = require('expect.js'),
+  path = require('path'),
+  Agenda = require(path.join('..', 'index.js'));
+
+var r = require('./fixtures/connection');
+
+
+// create agenda instances
+var jobs = new Agenda({
+  rethinkdb: r,
+  db: {
+    table: 'agendaJobs'
+  }
+});
+
+function clearJobs(done) {
+  r.table('agendaJobs').delete().run(done);
+}
+
+// Slow timeouts for travis
+var jobTimeout = process.env.TRAVIS ? 1500 : 300;
+
+
+describe('Once', function () {
+
+
+  describe(' run just once', function () {
+    this.timeout(20000);
+
+    beforeEach(clearJobs);
+
+    it(' should run the job only once', function (done) {
+      var startCounter = 0;
+
+      jobs.define('lock job', {
+        lockLifetime: 50
+      }, function (job, cb) {
+        startCounter++;
+      });
+
+      jobs.every('00 30 08 * * 2-6', 'lock job');
+      jobs.start();
+
+      setTimeout(function () {
+        jobs.jobs({name: 'lock job'}, function(err, job) {
+          job[0].run();
+        })
+      }, 10);
+
+      setTimeout(function () {
+        expect(startCounter).to.be(1);
+        jobs.stop(done);
+      }, 10000);
+
+    });
+  });
+
+});
